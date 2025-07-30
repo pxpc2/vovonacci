7cdd2f7524c3b24cc7c9be3dc0f6a30963577903
Pedro Daia <pedrodaia.c@gmail.com>
2025-07-30 02:39:33 -0300

    adding bg to all pages and posts

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -23,26 +23,29 @@ export default async function BlogPost(props: {
   const html = marked.parse(post.content);
 
   return (
-    <div>
-      <div className="flex font-sans font-medium items-center justify-center mx-32 pt-10 pb-8 border-b-1 border-gray-400">
-        <Link href="/" className="hover:underline">
-          <h1 className="text-2xl">vovonacci@PJT</h1>
-        </Link>
-      </div>
-
-      <article className="mx-12 flex flex-col items-center justify-center prose max-w-none dark:prose-invert">
-        <div className="flex">
-          <div className="flex flex-col py-10">
-            <h1 className="text-2xl font-bold">{post.title}</h1>
-            <p className="text-xl text-gray-300">{post.date}</p>
-          </div>
+    <div className="relative min-h-screen">
+      <div className="absolute inset-0 bg-[url('/wp.png')] bg-repeat-y  bg-top bg-center opacity-15 z-0" />
+      <div className="relative z-10">
+        <div className="flex font-sans font-medium items-center justify-center mx-32 pt-10 pb-8 border-b border-gray-400">
+          <Link href="/" className="hover:underline">
+            <h1 className="text-2xl">vovonacci@PJT</h1>
+          </Link>
         </div>
 
-        <div
-          className="markdown-body"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </article>
+        <article className="mx-12 flex flex-col items-center justify-center prose max-w-none dark:prose-invert">
+          <div className="flex">
+            <div className="flex flex-col py-10">
+              <h1 className="text-2xl font-bold">{post.title}</h1>
+              <p className="text-xl text-gray-300">{post.date}</p>
+            </div>
+          </div>
+
+          <div
+            className="markdown-body"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </article>
+      </div>
     </div>
   );
 }
